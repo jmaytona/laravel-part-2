71442cc63f404c71ffe0f7b9d46b58b2d48eb52d
JM <johnmichaelmaytona@gmail.com>
2019-04-01 21:33:27 +0800

    DB Migration, Resources Controller and Routes

--- routes/web.php
+++ routes/web.php
@@ -31,4 +31,6 @@ Route::get('/', 'PagesController@index');
 Route::get('/about', 'PagesController@about');
 Route::get('/service', 'PagesController@service');
 
+Route::resource('posts', 'PagesController');
+
 
